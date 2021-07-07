837d22dd590c31e4a8f760701e2a66cc62450246
Lênin Rocha <lenin0555@hotmail.com>
2021-07-07 18:55:47 -0300

    adição do dashboard
    adição da pagina principal

--- sistema-cadastro/src/pages/Dashboard/index.js
+++ sistema-cadastro/src/pages/Dashboard/index.js
@@ -1,14 +1,35 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
+import { useState } from 'react';
 import Header from '../../components/Header';
+import './dashboard.css'
+import Title from '../../components/Title'
+import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 export default function Dashboard(){
-  const { signOut } = useContext(AuthContext);
+
+  const [chamados, setChamados] = useState([]);
 
   return(
     <div>
       <Header/>
-      <h1>Farias me mama</h1>
+        <div className='content'>
+          <Title name="Atendimendos">
+            <FiMessageSquare size={25}/>
+          </Title>
+          {chamados.length === 0 ? (
+            <div  className='container dashboard'>
+            <span>Nenhum chamado registrado...</span>
+            <Link to='/new' className='new'>
+              <FiPlus size={25} color='#FFF' />
+              Novo chamado
+            </Link>
+
+          </div>
+          ) : (
+            <>
+            </>           )}
+          
+        </div>
     </div>
   )
 }
